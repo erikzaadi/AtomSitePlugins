d5ec0842c9ad8c367c9869aacc9f1a79b914c166
erikzaadi <erik.zaadi@gmail.com>
2009-11-09 19:41:14 +0200

    Added initial commit for TwitterPlugin

---
+++ src/TwitterPlugin/TwitterPlugin/TwitterController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AtomSite.WebCore;
+using System.Web.Mvc;
+
+namespace TwitterPlugin
+{
+    public class TwitterController : AtomSite.WebCore.BaseController
+    {
+        [ScopeAuthorize]
+        public ActionResult Setup()
+        {
+            return View();
+        }
+
+
+        [ScopeAuthorize]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Setup(string id)
+        {
+            return View(id);
+        }
+
+
+    }
+}
---
+++ src/TwitterPlugin/TwitterPlugin/TwitterPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AtomSite.WebCore;
+
+namespace TwitterPlugin
+{
+    public class TwitterPlugin : AtomSite.WebCore.BasePlugin
+    {
+        public TwitterPlugin(ILogService logger)
+            : base(logger)
+        {
+        }
+        public override void Register(StructureMap.IContainer container, List<AtomSite.WebCore.SiteRoute> routes, System.Web.Mvc.ViewEngineCollection viewEngines, System.Web.Mvc.ModelBinderDictionary modelBinders, ICollection<AtomSite.Domain.Asset> globalAssets)
+        {
+            RegisterWidget<TwitterWidget>(container);
+        }
+    }
+}
